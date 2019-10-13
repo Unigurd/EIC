15ac43c06d4199d656470794efded18e980d50ee
Unigurd <Sigurddam@hotmail.com>
2019-10-13 22:37:23 +0200

    Task0 finished

--- ECG_Solution/src/Main.cpp
+++ ECG_Solution/src/Main.cpp
@@ -4,7 +4,8 @@
 * This file is part of the ECG Lab Framework and must not be redistributed.
 */
 
-
+#include <string>
+#include <sstream>
 #include <GL\glew.h>
 #include <GLFW/glfw3.h>
 #include "Utils.h"
@@ -25,8 +26,104 @@
 // Callbacks
 /* --------------------------------------------- */
 
+static std::string FormatDebugOutput(GLenum source, GLenum type, GLuint id, GLenum severity, const char* msg) {
+    std::stringstream stringStream;
+    std::string sourceString;
+    std::string typeString;
+    std::string severityString; 
+    
+    switch (source) {
+        case GL_DEBUG_SOURCE_API: {
+            sourceString = "API";
+            break;
+        }
+        case GL_DEBUG_SOURCE_APPLICATION: {
+            sourceString = "Application";
+            break;
+        }
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM: {
+            sourceString = "Window System";
+            break;
+        }
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: {
+            sourceString = "Shader Compiler";
+            break;
+        }
+        case GL_DEBUG_SOURCE_THIRD_PARTY: {
+            sourceString = "Third Party";
+            break;
+        }
+        case GL_DEBUG_SOURCE_OTHER: {
+            sourceString = "Other";
+            break;
+        }
+        default: {
+            sourceString = "Unknown";
+            break;
+        }
+    }
+ 
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR: {
+            typeString = "Error";
+            break;
+        }
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: {
+            typeString = "Deprecated Behavior";
+            break;
+        }
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: {
+            typeString = "Undefined Behavior";
+            break;
+        }
+        case GL_DEBUG_TYPE_PORTABILITY_ARB: {
+            typeString = "Portability";
+            break;
+        }
+        case GL_DEBUG_TYPE_PERFORMANCE: {
+            typeString = "Performance";
+            break;
+        }
+        case GL_DEBUG_TYPE_OTHER: {
+            typeString = "Other";
+            break;
+        }
+        default: {
+            typeString = "Unknown";
+            break;
+        }
+    }
+ 
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH: {
+            severityString = "High";
+            break;
+        }
+        case GL_DEBUG_SEVERITY_MEDIUM: {
+            severityString = "Medium";
+            break;
+        }
+        case GL_DEBUG_SEVERITY_LOW: {
+            severityString = "Low";
+            break;
+        }
+        default: {
+            severityString = "Unknown";
+            break;
+        }
+    }
+ 
+    stringStream << "OpenGL Error: " << msg;
+    stringStream << " [Source = " << sourceString;
+    stringStream << ", Type = " << typeString;
+    stringStream << ", Severity = " << severityString;
+    stringStream << ", ID = " << id << "]";
+ 
+    return stringStream.str();
+}
 
-// To be changed
+
+
 void error_callback(int error, const char* description)
 {
 	fprintf(stderr, "Error: %s\n", description);
@@ -36,6 +133,13 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+static void APIENTRY DebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+                                   GLsizei length, const GLchar* message, const GLvoid* userParam) 
+{
+    std::string error = FormatDebugOutput(source, type, id, severity, message);
+    std::cout << error << std::endl;
 }
 
 
@@ -71,6 +175,12 @@ int main(int argc, char** argv)
 		EXIT_WITH_ERROR("Failed to init GLFW")
 	}
 
+    #if _DEBUG
+        // Create a debug OpenGL context or tell your OpenGL library (GLFW, SDL) to do so.
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    #endif 
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
@@ -91,6 +201,15 @@ int main(int argc, char** argv)
 		EXIT_WITH_ERROR("Failed to init GLEW")
 	}
 
+    #if _DEBUG
+    	// Register your callback function.
+    	glDebugMessageCallback(DebugCallback, NULL);
+    	// Enable synchronous callback. This ensures that your callback function is called
+    	// right after an error has occurred. 
+    	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    #endif
+  
+
 	glfwSwapInterval(1);
 
 	
@@ -105,10 +224,15 @@ int main(int argc, char** argv)
 	// Initialize scene and render loop
 	/* --------------------------------------------- */
 
+
+	glClearColor(1, 1, 1, 1);
 	
 	while (!glfwWindowShouldClose(window))
 	{	
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glfwPollEvents();
+		drawTeapot();
+		glfwSwapBuffers(window);
 	}
 
 
